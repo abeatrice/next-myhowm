0da1cb29dfbda0a1c86df099e375222a306fb80f
andrew beatrice <abeatrice.mail@gmail.com>
2021-02-23 15:09:40 -0800

    get recipes of signed in user

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Cookies} from 'react-cookie'
 import Head from 'next/head'
 import axios from 'axios'
 import {Typography, CardHeader, CardContent, Card} from '@material-ui/core'
@@ -16,11 +15,11 @@ function Page({recipes}) {
         Recipes
       </Typography>
       {
-        recipes.map(recipe => {
+        recipes.map((recipe, index) => {
           return (
-            <Card key={recipe.ID}>
+            <Card key={index}>
               <CardHeader 
-                title={recipe.Name}
+                title={recipe.RecipeName}
               />
               <CardContent>
                 {recipe.Description}
@@ -35,8 +34,7 @@ function Page({recipes}) {
 
 export async function getServerSideProps(context) {
   await authenticate(context)
-  const cookies = new Cookies()
-  const token = cookies.get('token')
+  const token = context.req.headers.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
   const res = await axios.get('http://localhost:3000/recipes', { headers: { 'Authorization': 'Bearer ' + token } })
   return {
     props: {recipes: res.data.data}
